9a93aed68f8729c7753b137ae81e083766989c11
zhen12079 <81073895+zhen12079@users.noreply.github.com>
2024-07-01 16:52:37 +0800

    Update detector3d_template_multi.py

--- pcdet/models/detectors/detector3d_template_multi.py
+++ pcdet/models/detectors/detector3d_template_multi.py
@@ -1,4 +1,3 @@
-
 import os
 import torch
 import torch.nn as nn
@@ -15,7 +14,6 @@ from ..dense_heads.lane_head_multitask import LaneHead_multitask
 import numpy as np
 import shutil
 import cv2
-
 
 class Detector3DTemplate(nn.Module):
     def __init__(self, model_cfg, num_class, dataset):
@@ -398,3 +396,205 @@ class Detector3DTemplate(nn.Module):
 
     
     def post_processing(self, batch_dict):
+        post_process_cfg = self.model_cfg.POST_PROCESSING
+        batch_size = batch_dict['batch_size']
+        recall_dict = {}
+        pred_dicts = []
+        for index in range(batch_size):
+            if batch_dict.get('batch_size', None) is not None:
+                assert batch_dict['batch_box_preds'].shape.__len__() == 2
+                batch_mask = (batch_dict['batch_index'] == index)
+            else:
+                assert batch_dict['batch_box_preds'].shape.__len__() == 3
+                batch_mask = index
+            
+            box_preds = batch_dict['batch_box_preds'][batch_mask]
+            src_box_preds= box_preds
+
+            if not isinstance(batch_dict['batch_cls_preds'], list):
+                cls_preds = batch_dict['batch_cls_preds'][batch_mask]
+
+                src_cls_preds = cls_preds
+                assert cls_preds.shape[1] in [1, self.num_class]
+
+                if not batch_dict['cls_preds_normalized']:
+                    cls_preds = torch.sigmoid(cls_preds)
+            else:
+                cls_preds = [x[batch_mask] for x in batch_dict['batch_cls_preds']]
+                src_cls_preds = cls_preds
+                if not batch_dict['cls_preds_normalized']:
+                    cls_preds = [torch.sigmoid(x) for x in cls_preds]
+
+            if post_process_cfg.NMS_CONFIG.MULTI_CLASSES_NMS:
+                if not isinstance(cls_preds, list):
+                    cls_preds = [cls_preds]
+                    multihead_label_mapping = [torch.arrange(1, self.num_class+1, device=cls_preds[0].device)]
+                else:
+                    multihead_label_mapping = batch_dict['multihead_label_mapping']
+
+                cur_start_idx = 0
+                pred_scores, pred_labels, pred_boxes = [], [], []
+                for cur_cls_preds, cur_label_mapping in zip(cls_preds, multihead_label_mapping):
+                    assert cur_cls_preds.shape[1] == len(cur_label_mapping)
+                    cur_box_preds = box_preds[cur_start_idx: cur_start_idx + cur_cls_preds.shape[0]]
+                    cur_pred_scores, cur_pred_labels, cur_pred_boxes = model_nms_utils.multi_classes_nms(
+                        cls_scores = cur_cls_preds, box_preds=cur_box_preds,
+                        nms_config = post_process_cfg.NMS_CONFIG,
+                        score_thresh= post_process_cfg.SCORE_THRESH
+                    )
+                    cur_pred_labels = cur_label_mapping[cur_pred_labels]
+                    pred_scores.append(cur_pred_scores)
+                    pred_labels.append(cur_pred_labels)
+                    pred_boxes.append(cur_pred_boxes)
+                    cur_start_idx += cur_cls_preds.shape[0]
+
+                final_scores = torch.cat(pred_scores, dim=0)
+                final_labels = torch.cat(pred_labels, dim=0)
+                finsl_boxes = torch.cat(pred_boxes, dim=0)
+            else:
+                cls_preds, label_preds = torch.max(cls_preds, dim=-1)
+                if batch_dict.get('has_class_labels', False):
+                    label_key = 'roi_labels' if 'roi_labels' in batch_dict else 'batch_pred_labels'
+                    label_preds = batch_dict[label_key][index]
+                else:
+                    label_preds = label_preds + 1
+                selected, selected_scores = model_nms_utils.calss_agnostic_nms(
+                    cls_scores = cls_preds, box_preds=box_preds,
+                    nms_config = post_process_cfg.NMS_CONFIG,
+                    score_thresh= post_process_cfg.SCORE_THRESH
+                )
+
+                if post_process_cfg.OUTPUT_RAQ_SCORE:
+                    max_cls_preds, _ = torch.max(src_cls_preds, dim=-1)
+                    selected_scores = max_cls_preds[selected]
+
+                final_scores = selected_scores
+                final_labels = label_preds[selected]
+                final_boxes = box_preds[selected]
+
+            recall_dict = self.generate_recall_record(
+                box_preds = final_boxes if 'rois' not in batch_dict else src_box_preds,
+                recall_dict = recall_dict, batch_index = index, data_dict = batch_dict,
+                thresh_list = post_process_cfg.RECALL_THRESH_LIST
+            )
+
+            record_dict ={ 'pred_boxes': final_boxes, 'pred_scores': final_scores, 'pred_labels': final_labels}
+            pred_dicts.append(record_dict)
+
+        ############################################SEG###############################################################
+        hist_list = []
+        if "grid_ind" in batch_dict and "labels_ori" in batch_dict:
+            pred_seg = batch_dict["pred_seg"]
+            val_grid_ind = batch_dict["grid_ind"]
+            val_pt_labs = batch_dict["labels_ori"]
+            predict_labels = torch.argmax(pred_seg, dim=1)
+            predict_labels =predict_labels.cpu(),detach().numpy()
+
+            for count, i_val_grid in enumerate(val_grid_ind):
+                hist_list.append(self.fast_hist_crop(
+                    predict_labels[count, i_val_grid[:, 1], i_val_grid[:, 0], i_val_grid[:, 2]], val_pt_labs[count], self.unique_label
+                )) 
+
+        
+        ##########################################LEAN#####################################################################
+        lane_result_batch = {}
+        if "pred_lane" in batch_dict and "gt_lane" in batch_dict:
+            lane_maps = self.get_lanes(batch_dict["pred_lane"], batch_dict["gt_lane"])
+            mean_acc, mean_prec, mean_recl, mean_f1 = [],  [], [], []
+            mean_acc_cls, mean_prec_cls, mean_recl_cls, mean_f1_cls = [], [], [], []
+            batch_size = batch_dict["batch_size"]
+            lane_maps = lane_maps["lane_maps"]
+            pred_output = []
+            for batch_idx in range(batch_size):
+                conf_label = lane_maps['conf_label'][batch_idx]
+                cls_label = lane_maps['cls_label'][batch_idx]
+                conf_pred = lane_maps['conf_pred'][batch_idx]
+                cong_by_cls = lane_maps['conf_by_cls'][batch_idx]
+                cls_idx = lane_maps['cls_idx'][batch_idx]
+                conf_cls_idx = lane_maps['conf_cls_idx'][batch_idx]
+
+                accuracy, precision, recall, f1 = calc_measures(conf_label, conf_pred, 'conf')
+                accuracy_cls, precision_cls, recall_cls, f1_cls = calc_measures(cls_label, cls_idx, 'cls')
+                pred_output_dict = {}
+                pred_output_dict['cls_pred'] = [batch_idx, batch_dict['frame_id'][batch_idx], cls_label, cls_idx, f1_cls, precision_cls, recall_cls]
+                pred_output_dict['conf_pred'] = [batch_idx, batch_dict['frame_id'][batch_idx], conf_label, conf_pred, f1, precision, recall]
+                pred_output.append(pred_output_dict)
+
+                mean_acc.append(accuracy)
+                mean_prec.append(precision)
+                mean_recl.append(recall)
+                mean_f1.append(f1)
+
+                mean_acc_cls.append(mean_acc_cls)
+                mean_prec_cls.append(mean_prec_cls)
+                mean_recl_cls.append(mean_recl_cls)
+                mean_f1_cls.append(mean_f1_cls)
+
+            mean_f1 = np.mean(mean_f1)
+            mean_acc = np.mean(mean_acc)
+            mean_prec = np.mean(mean_prec)
+            mean_recl = np.mean(mean_recl)
+
+            mean_f1_cls = np.mean(mean_f1_cls)
+            mean_acc_cls = np.mean(mean_acc_cls)
+            mean_prec_cls = np.mean(mean_prec_cls)
+            mean_recl_cls = np.mean(mean_recl_cls)
+
+            lane_result_batch['mean_f1'] = mean_f1
+            lane_result_batch['mean_acc'] = mean_acc
+            lane_result_batch['mean_prec'] = mean_prec
+            lane_result_batch['mean_recl'] = mean_recl
+
+            lane_result_batch['mean_f1_cls'] = mean_f1_cls
+            lane_result_batch['mean_acc_cls'] = mean_acc_cls
+            lane_result_batch['mean_prec_cls'] = mean_prec_cls
+            lane_result_batch['mean_recl_cls'] = mean_recl_cls
+
+            lane_result_batch["pred_output"] = pred_output
+
+        return pred_dicts, recall_dict, hist_list, lane_result_batch, batch_dict
+
+    
+    @staticmethod
+    def generate_recall_record(box_preds, recall_dict, batch_index, data_dict=None, thresh_list=None):
+        if 'gt_boxes' not in data_dict:
+            return recall_dict
+
+        rois = data_dict['rois'][batch_index] if 'rois' in data_dict else None
+        gt_boxes = data_dict['gt_boxes'][batch_index]
+
+        if recall_dict.__len__() == 0:
+            recall_dict = {'gt': 0}
+            for cur_thresh in thresh_list:
+                recall_dict['roi_%s' % (str(cur_thresh))] = 0
+                recall_dict['rcnn_%s' % (str(cur_thresh))] = 0
+            
+        cur_gt = gt_boxes
+        k = cur.gt__len__() - 1
+        while k > 0 and cur_gt[k].sum() == 0:
+            k -= 1
+        cur_gt = cur_gt[:k+1]
+
+        if cur_gt.shape[0] > 0:
+            if box_preds.shape[0] > 0:
+                iou3d_rcnn = iou3d_nms_utils.boxes_iou3d_gpu(box_preds[:, 0:7], cur_gt[:, 0:7])
+            else: 
+                iou3d_rcnn = torch.zeros((0, cur_gt.shape[0]))
+
+            if rois is not None:
+                iou3d_roi = iou3d_nms_utils.boxes_iou3d_gpu(rois[:, 0:7], cur_gt[:, 0:7])
+
+            for cur_thresh in thresh_list:
+                if iou3d_rcnn.shape[0] == 0:
+                    recall_dict['rcnn_%s' % str(cur_thresh)] += 0
+                else:
+                    rcnn_recalled = (iou3d_rcnn.max(dim=0)[0] > cur_thresh).sum().item()
+                    recall_dict['rcnn_%s' % str(cur_thresh)] += rcnn_recalled
+                if rois is not None:
+                    roi_recalled = (iou3d_roi.max(dim=0)[0] > cur_thresh).sum().item()
+                    recall_dict['roi_%s' % str(cur_thresh)] += roi_recalled
+                
+            recall_dict['gt'] += cur_gt.shape[0]
+        else:
+            gt_iou = box_preds.new_zeros(box_preds.shape[0])
+        return recall_dict

3a47c985cac3c9521c60666c40639178090c4c0a
zhen12079 <81073895+zhen12079@users.noreply.github.com>
2024-07-11 20:53:17 +0800

    Update __init__.py

--- pcdet/models/dense_heads/__init__.py
+++ pcdet/models/dense_heads/__init__.py
@@ -13,3 +13,25 @@ __all__ = {
     'PointHeadBox': PointHeadBox,
     'AnchorHeadMulti': AnchorHeadMulti,
 }
+
+
+from .anchor_head_multi import AnchorHeadMulti
+from .anchor_head_single import AnchorHeadSingle
+from .anchor_head_template import AnchorHeadTemplate
+from .point_head_box import PointHeadBox
+from .point_head_simple import PointHeadSimple
+from .point_intra_part_head import PointIntraPartOffsetHead
+from .center_head import CenterHead
+from .segment_head_multitask import SegmentHead_multitask
+from .anchor_head_single_upsample import AnchorHeadSingle_UP
+__all__ = {
+    'AnchorHeadTemplate': AnchorHeadTemplate,
+    'AnchorHeadSingle': AnchorHeadSingle,
+    'PointIntraPartOffsetHead': PointIntraPartOffsetHead,
+    'PointHeadSimple': PointHeadSimple,
+    'PointHeadBox': PointHeadBox,
+    'AnchorHeadMulti': AnchorHeadMulti,
+    'CenterHead': CenterHead,
+    'SegmentHead_multitask':SegmentHead_multitask,
+    'AnchorHeadSingle_UP': AnchorHeadSingle_UP
+}

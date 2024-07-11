b967f0d810b8f5d389860108b939e7a45c5beccf
zhen12079 <81073895+zhen12079@users.noreply.github.com>
2024-07-11 20:39:42 +0800

    Update __init__.py

--- pcdet/models/backbones_2d/map_to_bev/__init__.py
+++ pcdet/models/backbones_2d/map_to_bev/__init__.py
@@ -7,3 +7,16 @@ __all__ = {
     'PointPillarScatter': PointPillarScatter,
     'Conv2DCollapse': Conv2DCollapse
 }
+
+from .height_compression import HeightCompression
+from .pointpillar_scatter import PointPillarScatter
+from .pointpillar_scatter_lane import PointPillarScatter_lane
+from .conv2d_collapse import Conv2DCollapse
+
+__all__ = {
+    'HeightCompression': HeightCompression,
+    'PointPillarScatter': PointPillarScatter,
+    'PointPillarScatter_lane': PointPillarScatter_lane,
+    'Conv2DCollapse': Conv2DCollapse
+}
+
